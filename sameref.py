660c8f16a4e069c082f2f6a9cb2b168b621dcb6e
gitferry <greferry@gmail.com>
2018-11-26 16:56:07 -0800

    initial commit

---
+++ sameref.py
@@ -0,0 +1,7 @@
+from refextract import extract_references_from_file
+import os
+
+# TODO: read files from a directory path
+# TODO: extract references from each file
+# TODO: extract titles from each file
+# TODO: use a dic to store a "title -> count" mapping
